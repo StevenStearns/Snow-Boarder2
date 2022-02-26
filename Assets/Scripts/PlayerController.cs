49af504e6a81988898eec894c6d783f9332471cf
steven stearns <stearn102@morrisville.edu>
2022-02-25 23:02:34 -0500

    Triggers restart level.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour
         {
             rb2d.AddTorque(fltTorqueAmount);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+            else if(Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-fltTorqueAmount);
         }
