634bf43cc0a23c583f843fc76ea697303b864e7e
steven stearns <stearn102@morrisville.edu>
2022-03-01 08:40:25 -0500

    Using findObjectType

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,42 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     [SerializeField] float fltTorqueAmount = 1f;
+    [SerializeField] float fltBoostSpeed = 30f;
+    [SerializeField] float fltBaseSpeed = 20f;
+    SurfaceEffector2D surfaceEffector2D;
+   
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // move character left and right while in the air.
-        if(Input.GetKey(KeyCode.LeftArrow))
+        RotatePlayer();
+        RespondToBoost();
+      
+    } // void update
+
+    void RespondToBoost()
+    {
+        // if we push up speed up
+        // otherwise stay at normal speed
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = fltBoostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = fltBaseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+         if(Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(fltTorqueAmount);
         }
